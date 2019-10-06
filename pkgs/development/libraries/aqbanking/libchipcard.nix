5b12f5a25e5371596f1c2b14fe96197848fd2b97
Andreas Rammhold <andreas@rammhold.de>
2019-10-06 22:12:36 +0200

    experimental aqbanking bump

--- pkgs/development/libraries/aqbanking/libchipcard.nix
+++ pkgs/development/libraries/aqbanking/libchipcard.nix
@@ -6,12 +6,9 @@ in stdenv.mkDerivation rec {
   pname = "libchipcard";
   inherit version;
 
-  src = let
-    qstring = "package=02&release=${releaseId}&file=01";
-    mkURLs = map (base: "${base}/sites/download/download.php?${qstring}");
-  in fetchurl {
+  src = fetchurl {
     name = "${pname}-${version}.tar.gz";
-    urls = mkURLs [ "http://www.aquamaniac.de" "http://www2.aquamaniac.de" ];
+    url = "https://www.aquamaniac.de/rdm/attachments/download/${releaseId}/${pname}-${version}.tar.gz";
     inherit sha256;
   };
 
