fd4ff9b6e54f9db7a706f6595c20e6ddd2735c54
Moritz Angermann <moritz.angermann@gmail.com>
2021-01-09 21:38:45 +0800

    Add crossPkgs.aarch64-darwin

--- lib/systems/examples.nix
+++ lib/systems/examples.nix
@@ -218,6 +218,12 @@ rec {
     useiOSPrebuilt = true;
   };
 
+  aarch64-darwin = {
+    config = "aarch64-apple-darwin";
+    xcodePlatform = "MacOSX";
+    platform = {};
+  };
+
   #
   # Windows
   #
